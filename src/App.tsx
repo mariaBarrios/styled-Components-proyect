8bbaf30726013c55d8b562bc58b8883d60a9d1f4
maria_barrios <maria.barrios@biko2.com>
2020-10-31 01:29:31 +0100

    Añadir Componentes al App.tx y fuentes al index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
 import React from "react";
 
+import { GlobalStyles } from "./ui/theme/GlobalStyles";
+import { Container } from "./views/_components/Container";
+import {
+  AppName,
+  Header,
+  HeaderLogo,
+  LogoWrapper,
+} from "./views/_components/Header/Header";
+
+import {
+  SearchBox,
+  SearchButton,
+  SearchInput,
+} from "./views/_components/SearchBox";
+import { GoSearch } from "react-icons/go";
+
+import { memes } from "../src/services/Memes.json";
+import { MemeList } from "./views/MemeList/MemeList";
+import { colors } from "./ui/theme/theme";
+
 function App() {
   return (
-    <div>
-      <header>Hola Mundo</header>
-    </div>
+    <Container>
+      <GlobalStyles />
+      <Header>
+        <LogoWrapper>
+          <HeaderLogo />
+          <AppName>GUIFAFFINITY</AppName>
+        </LogoWrapper>
+      </Header>
+
+      <SearchBox>
+        <SearchInput />
+        <SearchButton>
+          <GoSearch color={colors.white} />
+        </SearchButton>
+      </SearchBox>
+
+      <MemeList memes={memes} />
+    </Container>
   );
 }
 
